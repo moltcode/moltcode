90efe87cba2cdac1cef2d50efd641908f26d0b15
J <junaid1460@gmail.com>
2026-02-08 20:02:29 +0530

    Update download URL for version 0.0.3

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 
 export const config = {
   version: "0.0.2",
-  downloadUrl: "https://github.com/moltcode/moltcode/releases/download/0.0.2/MoltCode.dmg",
+  downloadUrl: "https://github.com/moltcode/moltcode/releases/download/0.0.3/MoltCode.dmg",
   githubUrl: "https://github.com/moltcode/moltcode",
 };
