f5d300a1105b111a59d88cdc6cc9b0688f7d1faa
J <junaid1460@gmail.com>
2026-02-10 14:15:58 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // App configuration - update these values manually when releasing new versions
 
 export const config = {
-  version: "0.0.3",
-  downloadUrl: "https://github.com/moltcode/moltcode/releases/download/0.0.3/MoltCode.dmg",
+  version: "0.0.8",
+  downloadUrl: "https://github.com/moltcode/moltcode/releases/download/v0.0.8/MoltCode.dmg",
   githubUrl: "https://github.com/moltcode/moltcode",
 };
