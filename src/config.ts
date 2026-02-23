87a571e2098f08e664e9ce00dd19ba339f27c9e9
J <junaid1460@gmail.com>
2026-02-23 12:31:15 +0530

    Update version and download URL to 0.0.71

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // App configuration - update these values manually when releasing new versions
 
 export const config = {
-  version: "0.0.70",
-  downloadUrl: "https://github.com/moltcode/moltcode/releases/download/v0.0.70/MoltCode.dmg",
+  version: "0.0.71",
+  downloadUrl: "https://github.com/moltcode/moltcode/releases/download/v0.0.71/MoltCode.dmg",
   githubUrl: "https://github.com/moltcode/moltcode",
 };
