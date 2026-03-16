cb52fe02758a7542c9c1c4aae330e8ca1077699a
J <junaid1460@gmail.com>
2026-03-17 00:48:55 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // App configuration - update these values manually when releasing new versions
 
 export const config = {
-  version: "0.0.86",
-  downloadUrl: "https://github.com/moltcode/moltcode/releases/download/v0.0.86/MoltCode-darwin-arm.dmg",
-  downloadUrlIntel: "https://github.com/moltcode/moltcode/releases/download/v0.0.86/MoltCode-darwin-intel.dmg",
+  version: "0.0.88",
+  downloadUrl: "https://github.com/moltcode/moltcode/releases/download/v0.0.88/MoltCode-darwin-arm.dmg",
+  downloadUrlIntel: "https://github.com/moltcode/moltcode/releases/download/v0.0.88/MoltCode-darwin-intel.dmg",
 };
