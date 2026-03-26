51674cf97d0504fa2f32969f22c3617796c45559
J <junaid1460@gmail.com>
2026-03-26 14:15:22 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 
 export const config = {
   version: "0.0.99",
-  downloadUrl: "https://github.com/moltcode/moltcode/releases/download/v0.0.99/MoltCode-darwin-arm.dmg",
-  downloadUrlIntel: "https://github.com/moltcode/moltcode/releases/download/v0.0.99/MoltCode-darwin-intel.dmg",
+  downloadUrl: "https://github.com/moltcode/moltcode/releases/download/v0.0.100/MoltCode-darwin-arm.dmg",
+  downloadUrlIntel: "https://github.com/moltcode/moltcode/releases/download/v0.0.100/MoltCode-darwin-intel.dmg",
   appStoreUrl: "https://apps.apple.com/in/app/molt-code/id6760204854",
 };
