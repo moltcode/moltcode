54da5216e023c24c221a1f0b8c793d85f46aea57
J <junaid1460@gmail.com>
2026-03-30 18:40:10 +0530

    Update version number to 0.0.105

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // App configuration - update these values manually when releasing new versions
 
 export const config = {
-  version: "0.0.101",
+  version: "0.0.105",
   downloadUrl: "https://github.com/moltcode/moltcode/releases/download/v0.0.105/MoltCode-darwin-arm.dmg",
   downloadUrlIntel: "https://github.com/moltcode/moltcode/releases/download/v0.0.105/MoltCode-darwin-intel.dmg",
   appStoreUrl: "https://apps.apple.com/in/app/molt-code/id6760204854",
