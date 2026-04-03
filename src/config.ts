d8c89009fe14651deea29bae310502dccfbc046a
J <junaid1460@gmail.com>
2026-04-03 18:28:51 +0530

    Update download URL for version 0.0.107

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 
 export const config = {
   version: "0.0.106",
-  downloadUrl: "https://github.com/moltcode/moltcode/releases/download/v0.0.106/MoltCode-darwin-arm.dmg",
+  downloadUrl: "https://github.com/moltcode/moltcode/releases/download/v0.0.107/MoltCode-darwin-arm.dmg",
   downloadUrlIntel: "https://github.com/moltcode/moltcode/releases/download/v0.0.106/MoltCode-darwin-intel.dmg",
   appStoreUrl: "https://apps.apple.com/in/app/molt-code/id6760204854",
 };
